13f3d07715dd266475373da9bb9dd6211ea829fd
rstiskalek <richard.stiskalek@protonmail.com>
2023-06-19 23:26:06 +0100

    add __init__

---
+++ gwlss/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2023 Richard Stiskalek
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from .sky import rand_rotation_matrix, rotate_radec  # noqa
+from .paths import Paths  # noqa
+
+from astropy.cosmology import FlatLambdaCDM
+
+gw170817 = {"RA": 197.45042353,
+            "dec": -23.38192721,
+            "redshift": 0.0099,
+            }
+paths_glamdring = {
+    "GW170817_darkPE" : "/mnt/extraspace/rstiskalek/GWLSS/H1L1V1-EXTRACT_POSTERIOR_GW170817-1187008600-400.hdf",  # noqa
+    "dumpdir": "/mnt/extraspace/rstiskalek/GWLSS/dumpdir",
+    }
+
+cosmo_csiborg = FlatLambdaCDM(H0=70.5, Om0=0.307, Ob0=0.04825, Tcmb0=2.728,
+                              Neff=3.046, m_nu=0.0, name='csiborg')
